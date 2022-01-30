732838f6867e6660222ad470fdd1d6eff9b61d64
Patrick Skinner <patrickskinner@outlook.com>
2022-01-30 17:32:27 +1300

    Added parent/child relationships for Objects

--- Object.js
+++ Object.js
@@ -1,10 +1,10 @@
 class Object{
-    transform;
-
     constructor(){
         this.transform = new Matrix4();
+        this.parent = null;
+        this.children = [];
     }
-    
+
     /**
      * Multiply the viewing matrix from the right.
      * @param eyeX, eyeY, eyeZ The position of the eye point.
@@ -13,7 +13,33 @@ class Object{
      * @return this
     */
     transformLookAt(eyeX, eyeY, eyeZ, centerX, centerY, centerZ, upX, upY, upZ){
-        console.log("parent");
         this.transform.lookAt(eyeX, eyeY, eyeZ, centerX, centerY, centerZ, upX, upY, upZ);
+    }
+
+    addChild(child){
+        var alreadyAdded = false;
+        this.children.forEach(element => {
+            if( element == child ) alreadyAdded = true;
+        });
+
+        if(!alreadyAdded){
+            if( child.parent == null ){ // New child doesn't already have a parent
+                child.parent = this;
+                this.children.push(child);
+            }
+            else { // Child has a parent already
+                child.parent.removeChild(child);
+                child.parent = this;
+                this.children.push(child);
+            }
+        }
+    }
+    
+    removeChild(child){      
+        const index = this.children.indexOf(child);
+        if (index > -1) {
+        this.children.splice(index, 1);
+          child.parent = null;
+        }
     }
 }
