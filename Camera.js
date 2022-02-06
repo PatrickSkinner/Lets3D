228d77bed09bb7ef3025133f2462689989f30d68
Patrick Skinner <patrickskinner@outlook.com>
2022-02-06 22:57:58 +1300

    Use transform directly for camera pose

--- Camera.js
+++ Camera.js
@@ -2,7 +2,7 @@ class Camera extends Object3D{
     projectionMatrix;
     mvpMatrix;
 
-    constructor(  fov, aspectRatio, nearClipping, farClipping ){
+    constructor(fov, aspectRatio, nearClipping, farClipping){
         super();
 
         // Set the viewing volume
@@ -11,11 +11,10 @@ class Camera extends Object3D{
         this.mvpMatrix = this.projectionMatrix;
     }
 
-    // TODO: Directly use object transform for matrix calculations.
     transformLookAt(eyeX, eyeY, eyeZ, centerX, centerY, centerZ, upX, upY, upZ){
         super.transformLookAt(eyeX, eyeY, eyeZ, centerX, centerY, centerZ, upX, upY, upZ);
         this.mvpMatrix = this.projectionMatrix;
-        this.mvpMatrix.lookAt(eyeX, eyeY, eyeZ, centerX, centerY, centerZ, upX, upY, upZ);
+        this.mvpMatrix.multiply(this.transform);
     }
 
 }
